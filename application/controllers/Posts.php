c3b8d2f847ebf63361a3d3eb415aa1b9c3b9d4c0
tomual <tomual@users.noreply.github.com>
2017-10-22 17:45:17 -0500

    Create Post

---
+++ application/controllers/Posts.php
@@ -0,0 +1,51 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Posts extends CI_Controller {
+
+public function __construct()
+{
+    parent::__construct();
+    $this->load->model('posts_model');
+    $this->load->library('form_validation');
+}
+
+    public function create() 
+    {
+        if (!$this->ion_auth->logged_in())
+        {
+            redirect('auth/login');
+        }
+
+
+        if($this->input->method() == 'post')
+        {
+            $this->form_validation->set_rules('title', 'Title', 'required');
+            $this->form_validation->set_rules('content', 'Content', 'required');
+
+            // Get user ID of current user
+            $author = $this->ion_auth->user()->row();
+            $author = $author->id;
+
+            // Get current date time
+            $publish_date = date("Y-m-d H:i:s");
+
+            // Put it all together
+            $data = array(
+                'title'     => $this->input->post('title'),
+                'content'   => $this->input->post('content'),
+                'author'    => $author,
+                'publish_date' => $publish_date
+            );
+
+            if($this->form_validation->run() !== FALSE)
+            {
+                // Create the post
+                $this->posts_model->create($data);
+            }
+        }
+
+        $this->load->view('posts/create');
+    }
+
+}
---
+++ application/views/posts/create.php
@@ -0,0 +1,11 @@
+<?php echo form_open() ?>
+	<label>Post Title</label><br>
+	<input type="text" name="title" value="<?php echo set_value('title') ?>"><br>
+	<?php echo form_error('title'); ?>
+
+	<label>Post Content</label><br>
+	<textarea name="content"><?php echo set_value('content') ?></textarea><br>
+	<?php echo form_error('content'); ?>
+
+	<input type="submit" value="Post">
+<?php echo form_close() ?>
